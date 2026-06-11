8f7b804837fb31d55b80d8f39b1dab0d1c486c9e
Jerbeson Rodrigues <jerbesonvinycius@gmail.com>
2026-06-11 19:38:40 -0300

    feat(login): create user repository

---
+++ src/infra/repositories/user.repository.ts
@@ -0,0 +1,33 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { GetUserByEmail } from '@/domain/contracts/repositories';
+import { UserEntity } from '@/domain/entities';
+import { MONGO_DB } from '../database/database.module';
+import { Db } from 'mongodb';
+
+@Injectable()
+export class UserRepository implements GetUserByEmail<UserEntity> {
+  private readonly collectionName = 'users';
+
+  constructor(
+    @Inject(MONGO_DB)
+    private readonly db: Db,
+  ) {}
+
+  async getUserByEmail(email: string): Promise<UserEntity | null> {
+    const user = await this.db
+      .collection(this.collectionName)
+      .findOne({ email });
+
+    if (!user) {
+      return null;
+    }
+
+    return new UserEntity({
+      id: user.id.toString(),
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      password: user.password,
+    });
+  }
+}

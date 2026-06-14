1d01630f90543ec970b4712047353d97e0dd0c7e
Jerbeson Rodrigues <jerbesonvinycius@gmail.com>
2026-06-14 12:27:23 -0300

    feat(guard): create tenant guard

--- src/infra/guards/index.ts
+++ src/infra/guards/index.ts
@@ -1,2 +1,3 @@
 export * from './jwt.guard';
 export * from './librarian.guard';
+export * from './tenant.guard';
---
+++ src/infra/guards/tenant.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
+import { decodeToken } from '@/shared';
+
+export class TenantGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers?.authorization.replace('Bearer ', '');
+    const decodedToken = decodeToken({ token });
+
+    if (!decodedToken || decodedToken?.role != 'TENANT') {
+      throw new HttpException('Tenant access is required.', 401);
+    }
+
+    return true;
+  }
+}

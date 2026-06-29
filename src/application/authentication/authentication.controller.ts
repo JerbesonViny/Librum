209b37934e0a8740deaf0218815583e68247fd64
Jerbeson Rodrigues <jerbesonvinycius@gmail.com>
2026-06-29 00:12:38 -0300

    doc: create auth swagger doc

--- src/application/authentication/authentication.controller.ts
+++ src/application/authentication/authentication.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import {
   AuthLibrarianUseCase,
   AuthTenantUseCase,
@@ -17,19 +17,177 @@ export class AuthenticationController {
   ) {}
 
   @Post('librarian')
-  @ApiOperation({ summary: 'Sign in with librarian account' })
+  @ApiOperation({ summary: 'Logar como bibliotecario' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', title: 'Email' },
+        password: { type: 'string', title: 'Senha' },
+      },
+    },
+    examples: {
+      valid: {
+        summary: 'Credenciais validas',
+        value: {
+          email: 'mockedLibrarianEmail',
+          password: 'mockedLibrarianPassword',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    content: {
+      'application/json': {
+        examples: {
+          success: {
+            summary: 'Retorna um JWT',
+            value: { token: 'jwt-token' },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    content: {
+      'application/json': {
+        examples: {
+          userNotFound: {
+            summary: 'Usuario nao cadastrado',
+            value: { message: 'User not found.' },
+          },
+          wrongPassword: {
+            summary: 'Senha incorreta',
+            value: { message: 'Wrong password.' },
+          },
+        },
+      },
+    },
+  })
   librarianLogin(@Body() input: LoginInput) {
     return this.authLibrarianUseCase.perform(input);
   }
 
   @Post('tenant')
-  @ApiOperation({ summary: 'Sign in with tenant account' })
+  @ApiOperation({ summary: 'Logar como locatario' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', title: 'Email' },
+        password: { type: 'string', title: 'Senha' },
+      },
+    },
+    examples: {
+      valid: {
+        summary: 'Credenciais validas',
+        value: {
+          email: 'mockedTenantEmail',
+          password: 'mockedTenantPassword',
+        },
+      },
+      wrongPassword: {
+        summary: 'Senha errada',
+        value: {
+          email: 'mockedTenantEmail',
+          password: 'wrongPassword',
+        },
+      },
+      userNotFound: {
+        summary: 'Usuario nao encontrado',
+        value: {
+          email: 'notFoundEmail',
+          password: 'notFoundPassword',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    content: {
+      'application/json': {
+        examples: {
+          success: {
+            summary: 'Retorna um JWT',
+            value: { token: 'jwt-token' },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    content: {
+      'application/json': {
+        examples: {
+          userNotFound: {
+            summary: 'Usuario nao cadastrado',
+            value: { message: 'User not found.' },
+          },
+          wrongPassword: {
+            summary: 'Senha incorreta',
+            value: { message: 'Wrong password.' },
+          },
+        },
+      },
+    },
+  })
   tenantLogin(@Body() input: LoginInput) {
     return this.authTenantUseCase.perform(input);
   }
 
   @Post('admin')
-  @ApiOperation({ summary: 'Sign in with admin account' })
+  @ApiOperation({ summary: 'Logar como admin' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: { type: 'string', title: 'Email' },
+        password: { type: 'string', title: 'Senha' },
+      },
+    },
+    examples: {
+      valid: {
+        summary: 'Credenciais validas',
+        value: {
+          email: 'mockedAdminEmail',
+          password: 'mockedAdminPassword',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    content: {
+      'application/json': {
+        examples: {
+          success: {
+            summary: 'Retorna um JWT',
+            value: { token: 'jwt-token' },
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    content: {
+      'application/json': {
+        examples: {
+          userNotFound: {
+            summary: 'Usuario nao cadastrado',
+            value: { message: 'User not found.' },
+          },
+          wrongPassword: {
+            summary: 'Senha incorreta',
+            value: { message: 'Wrong password.' },
+          },
+        },
+      },
+    },
+  })
   adminLogin(@Body() input: LoginInput) {
     return this.authAdminUseCase.perform(input);
   }
